9da83a5e031bb708fa4edf6472f23cc2655e4c8a
Corey <coreyshanahan01@gmail.com>
2022-10-09 02:02:28 +0100

    Charater Movement

---
+++ Assets/Scripts/CameraLook.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraLook : MonoBehaviour
+{
+    [SerializeField] private Transform orientaion;
+    [Range(0f, 10f)]
+    [SerializeField] private float mouseSensitivity = 1f;
+    private float mouseX;
+    private float mouseY;
+
+    private float rotationX;
+    private float rotationY;
+    
+    private Vector2 vector2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Look();
+    }
+
+    public void onLook(InputAction.CallbackContext callbackContext)
+    {
+        vector2 = callbackContext.ReadValue<Vector2>();
+        Debug.Log(vector2);
+    }
+
+    private void Look()
+    {
+        mouseX = vector2.x * Time.deltaTime * mouseSensitivity;
+        mouseY = vector2.y * Time.deltaTime * mouseSensitivity;
+
+        rotationX -= mouseY;
+        rotationY += mouseX;
+        rotationX = Math.Clamp(rotationX, -90f, 90f);
+        
+        transform.rotation = Quaternion.Euler(rotationX, rotationY,0);
+        orientaion.rotation = Quaternion.Euler(0, rotationY, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")] 
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float groundDrag;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float jumpCoolDown;
+    [SerializeField] private float airMultiplier;
+    private bool readyToJump;
+
+    [Header("Ground Check")] 
+    [SerializeField] private float playerHight;
+    [SerializeField] private LayerMask LayerMask;
+    private bool grounded;
+    
+
+    [SerializeField] private Transform orientation;
+    [SerializeField] private Rigidbody Rigidbody;
+
+    private float horizontalInput;
+    private float verticalInput;
+    private Vector3 moveDirection;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        readyToJump = true;
+    }
+
+    private void Update()
+    {
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHight * 0.5f + 0.2f, LayerMask);
+        
+        speedControl();
+        
+        if (grounded) Rigidbody.drag = groundDrag;
+        else Rigidbody.drag = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        move();
+    }
+
+    public void onMove(InputAction.CallbackContext callbackContext)
+    {
+        Vector2 vector = callbackContext.ReadValue<Vector2>();
+        horizontalInput = vector.x;
+        verticalInput = vector.y;
+    }
+
+    public void onJump(InputAction.CallbackContext callbackContext)
+    {
+        if (callbackContext.ReadValueAsButton() && readyToJump && grounded)
+        {
+            readyToJump = false;
+            
+            jump();
+            
+            Invoke(nameof(resetJump), jumpCoolDown);
+        }
+    }
+
+    private void move()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        if (grounded)
+        {
+            Rigidbody.AddForce(moveSpeed * 10f * moveDirection.normalized, ForceMode.Force);
+        }
+       else if (!grounded)
+        {
+            Rigidbody.AddForce(moveSpeed * 10f * airMultiplier * moveDirection.normalized, ForceMode.Force);
+        }
+       
+       
+    }
+
+    private void speedControl()
+    {
+        Vector3 flatVel = new Vector3(Rigidbody.velocity.x, 0f, Rigidbody.velocity.z);
+
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            Rigidbody.velocity = new Vector3(limitedVel.x, Rigidbody.velocity.y, limitedVel.z);
+        }
+    }
+
+    private void jump()
+    {
+        Rigidbody.velocity = new Vector3(Rigidbody.velocity.x, 0f, Rigidbody.velocity.z);
+        
+        Rigidbody.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void resetJump()
+    {
+        readyToJump = true;
+    }
+}
