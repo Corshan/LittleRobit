907b1542717bd9727702188979a72bf807bcbed7
Corey Shanahan <coreyshanahan01@gmail.com>
2022-10-27 19:11:33 +0100

    Animatations on guard + imported materials for AI

--- Assets/Scripts/AI/AIController.cs
+++ Assets/Scripts/AI/AIController.cs
@@ -9,10 +9,12 @@ public class AIController : MonoBehaviour
     [SerializeField] private List<Transform> _wayPoints;
 
     [SerializeField] private NavMeshAgent _agent;
+    [SerializeField] private Animator _animator;
     [SerializeField] private float minDistanceToWaypoint = 2f;
 
     private Transform currentWaypoint;
     private int waypointIndex;
+    private bool waitBool = false;
     
     // Start is called before the first frame update
     void Start()
@@ -24,12 +26,17 @@ public class AIController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        FollowWaypoints();
+        if (!waitBool)
+        {
+            FollowWaypoints();
+        }
+
+        
     }
 
     void FollowWaypoints()
     {
-
+        _animator.SetBool("walk_anim" ,true);
         _agent.SetDestination(currentWaypoint.position);
 
         float distance = Vector3.Distance(currentWaypoint.position, _agent.gameObject.transform.position);
@@ -45,7 +52,16 @@ public class AIController : MonoBehaviour
             }
 
             currentWaypoint = _wayPoints[waypointIndex];
+            StartCoroutine(wait());
         }
         //Debug.Log(distance);
+    }
+
+    IEnumerator wait()
+    {
+        waitBool = true;
+        _animator.SetBool("walk_anim" ,false);
+        yield return new WaitForSeconds(3);
+        waitBool = false;
     }
 }
