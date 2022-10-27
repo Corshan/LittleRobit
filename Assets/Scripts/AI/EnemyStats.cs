c36565d9bb39203c659deec9382314b450201559
Corey Shanahan <coreyshanahan01@gmail.com>
2022-10-27 19:54:26 +0100

    Updated main menu

--- Assets/Scripts/AI/EnemyStats.cs
+++ Assets/Scripts/AI/EnemyStats.cs
@@ -5,5 +5,10 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "ScriptableObjects/EnemyStats")]
 public class EnemyStats : ScriptableObject
 {
-    public float speed = 7f;
+    [Range(1f,10f)]
+    public float walkingSpeed = 7f;
+    [Range(1f,10f)]
+    public float runningSpped = 7f;
+    [Range(1f,5f)]
+    public float attackDistance = 1f;
 }
