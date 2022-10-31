42d75afbe8303a44c94293a888ac5e71dd8713f6
Corey <coreyshanahan01@gmail.com>
2022-10-31 18:07:32 +0000

    Menu settings working

--- Assets/Scripts/Events/ButtonEvents.cs
+++ Assets/Scripts/Events/ButtonEvents.cs
@@ -1,9 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class ButtonEvents : MonoBehaviour
 {
+    [SerializeField] private Animator _animator;
+    [SerializeField] private AudioMixer _audioMixer;
+    [SerializeField] private Slider _mouseSlider;
+    [SerializeField] private Slider _musicSlider;
+    [SerializeField] private Slider _SFXSlider;
+    [SerializeField] private playerStats _stats;
+
+    private void Start()
+    {
+        float value;
+        _audioMixer.GetFloat("musicVolume", out value);
+        _musicSlider.value = value;
+
+        _audioMixer.GetFloat("sfxVolume", out value);
+        _SFXSlider.value = value;
+
+        _mouseSlider.value = _stats.mouseSensitivity;
+    }
+
+    private void Update()
+    {
+        _audioMixer.SetFloat("musicVolume", _musicSlider.value);
+        _audioMixer.SetFloat("sfxVolume", _SFXSlider.value);
+        _stats.mouseSensitivity = _mouseSlider.value;
+    }
+
     public void startGame()
     {
         GameEvents.current.gameStart();
@@ -12,5 +41,15 @@ public class ButtonEvents : MonoBehaviour
     public void quitGame()
     {
         GameEvents.current.gameQuit();
+    }
+
+    public void openSettings()
+    {
+        _animator.SetBool("settings", true);
+    }
+
+    public void closeSettings()
+    {
+        _animator.SetBool("settings", false);
     }
 }
